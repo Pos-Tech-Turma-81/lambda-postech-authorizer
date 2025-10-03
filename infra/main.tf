5cdc15f0f81064a480977eea62db8ef74ffd4288
Ruano12 <34557800+Ruano12@users.noreply.github.com>
2025-10-02 22:39:10 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -62,20 +62,11 @@ resource "aws_security_group" "lambda_authorizer" {
   name        = "authorizer-sg"
   vpc_id      =  data.aws_vpc.postech_vpc.id
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "P"
-  }
-
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "P"
   }
 }
 
